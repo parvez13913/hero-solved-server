df59c73fe573e0bc3cf505ee3b31147d9805b442
parvez13913 <parvezz13913@gmail.com>
2022-07-01 04:54:12 -0700

    delete api complete

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const express = require("express");
 const app = express();
 require('dotenv').config();
 const cors = require('cors');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 const port = process.env.PORT || 5000;
 
 
@@ -10,6 +11,61 @@ app.use(express.json());
 
 
 
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.yabnr.mongodb.net/?retryWrites=true&w=majority`;
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+
+
+async function run() {
+    try {
+        await client.connect();
+        const todoCollection = client.db('HeroSolved').collection('todo');
+
+        app.post('/todo', async (req, res) => {
+            const todo = req.body;
+            const result = await todoCollection.insertOne(todo);
+            res.send(result);
+        });
+
+        app.get('/todo', async (req, res) => {
+            const quary = {};
+            const cursor = todoCollection.find(quary);
+            const todo = await cursor.toArray();
+            res.send(todo);
+        });
+
+        app.delete('/todo/:id', async (req, res) => {
+            const id = req.params.id;
+            const query = { _id: ObjectId(id) };
+            const todo = await todoCollection.deleteOne(query);
+            res.send(todo);
+        });
+
+        app.get('/todo/:id', async (req, res) => {
+            const id = req.params.id;
+            const query = { _id: ObjectId(id) };
+            const todo = await todoCollection.findOne(query);
+            res.send(todo);
+        })
+
+
+        // app.put('/todo/:id', async (req, res) => {
+        //     const id = req.params.id;
+        //     const todo = req.body;
+        //     const filter = { _id: ObjectId(id) };
+        //     const options = { upsert: true };
+        //     const updatedDoc = {
+        //         $set: todo,
+        //     };
+        //     const result = await todoCollection.updateOne(filter, updatedDoc, options);
+        //     res.send(result);
+        // });
+    }
+    finally {
+
+    }
+}
+
+run()
 
 app.get('/', async (req, res) => {
     res.send("HelloW Hero");
